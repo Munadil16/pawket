c6fec539438c992251f5e3e4f2581c345590f53e
Munadil16 <munadilxd@gmail.com>
2025-06-02 21:23:31 +0530

    chore: Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
